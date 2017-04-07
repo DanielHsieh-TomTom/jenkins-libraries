ac49ac1553acbb08482109f5812a7c9d92737501
Erik Vermunt <erik.vermunt@tomtom.com>
2017-04-07 18:55:52 +0200

    Add util method for getting test configuration based on job-name

--- src/com/tomtom/Utils.groovy
+++ src/com/tomtom/Utils.groovy
@@ -2,10 +2,37 @@ package com.tomtom;
 
 @NonCPS
 static def getP4Changelist(build) {
-    println "build: ${build}"
     def rawBuild = build.getRawBuild()
-    println "rawBuild: ${rawBuild}"
     def environment = rawBuild.getEnvironment()
-    println "environment: ${environment}"
     return environment["P4_CHANGELIST"]
+}
+
+@NonCPS
+static def getBuildConfig(jobName) {
+    def buildConfig = [:]
+
+    // Determine branch name
+    buildConfig['branchName'] = jobName.substring(jobName.indexOf('/') + 1)
+
+    // Determine test-suite and node-type
+    switch (jobName.split('/')[0]) {
+        case "fast-build":
+            if (buildConfig['branchName'] == 'develop') {
+                buildConfig['testSuite'] = 'jenkins_main_fast'
+            } else {
+                buildConfig['testSuite'] = 'jenkins_other_fast'
+            }
+            buildConfig['nodeType'] = 'fast && ttc'
+            break;
+        case "slow-build":
+            if (buildConfig['branchName'] == 'develop') {
+                buildConfig['testSuite'] = 'jenkins_main_slow'
+            } else {
+                buildConfig['testSuite'] = 'jenkins_other_slow'
+            }
+            buildConfig['nodeType'] = 'slow && ttc'
+            break;
+    }
+
+    return buildConfig
 }

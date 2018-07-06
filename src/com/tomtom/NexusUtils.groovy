f5ea0886a18f0f34003eea0d10a454cc441db170
Erik Vermunt <erik.vermunt@tomtom.com>
2018-07-06 16:26:35 +0200

    Add NexusUtils

---
+++ src/com/tomtom/NexusUtils.groovy
@@ -0,0 +1,24 @@
+package com.tomtom;
+
+@NonCPS
+static def getPublishingInfo(jobName, buildNumber, commit) {
+    def rootJob = URLDecoder.decode(jobName.substring(0, jobName.indexOf('/')))
+    def branchName = URLDecoder.decode(jobName.substring(jobName.indexOf('/') + 1))
+
+    def publishInfo = [publish: false]
+    if (rootJob == 'slow-build') {
+        switch (branchName) {
+            case "develop":
+                publishInfo['publish'] = true
+                publishInfo['repo'] = 'navapp-staging'
+                publishInfo['version'] = "develop-slow.${buildNumber}-${commit}"
+                break
+            case ~/rel-([0-9]{2}\.[0-9])/:
+                publishInfo['publish'] = true
+                publishInfo['repo'] = 'navapp-releases'
+                publishInfo['version'] = "${java.util.regex.Matcher.lastMatcher[0][1]}.${buildNumber}-${commit}"
+                break
+        }
+    }
+    return publishInfo
+}

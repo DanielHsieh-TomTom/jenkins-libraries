87a060350d4182238e5e876ff34e800e71abea63
Erik Vermunt <Erik.Vermunt@tomtom.com>
2019-05-02 21:58:39 +0200

    Update NexusUtils.getPublishInfo to handle custom-fast-build and custom-slow-build

--- src/com/tomtom/NexusUtils.groovy
+++ src/com/tomtom/NexusUtils.groovy
@@ -2,22 +2,25 @@ package com.tomtom;
 
 @NonCPS
 static def getPublishingInfo(jobName, buildNumber, commit) {
-    def rootJob = URLDecoder.decode(jobName.substring(0, jobName.indexOf('/')))
-    def branchName = URLDecoder.decode(jobName.substring(jobName.indexOf('/') + 1))
-
     def publishInfo = [publish: false]
-    if (rootJob == 'slow-build') {
-        switch (branchName) {
-            case "develop":
-                publishInfo['publish'] = true
-                publishInfo['repo'] = 'navapp-staging'
-                publishInfo['version'] = "develop-slow.${buildNumber}-${commit}"
-                break
-            case ~/rel-([0-9]{2}\.[0-9])/:
-                publishInfo['publish'] = true
-                publishInfo['repo'] = 'navapp-releases'
-                publishInfo['version'] = "${java.util.regex.Matcher.lastMatcher[0][1]}.${buildNumber}-${commit}"
-                break
+
+    if (jobName != "custom-fast-build" && jobName != "custom-slow-build") {
+        def rootJob = URLDecoder.decode(jobName.substring(0, jobName.indexOf('/')))
+        def branchName = URLDecoder.decode(jobName.substring(jobName.indexOf('/') + 1))
+
+        if (rootJob == 'slow-build') {
+            switch (branchName) {
+                case "develop":
+                    publishInfo['publish'] = true
+                    publishInfo['repo'] = 'navapp-staging'
+                    publishInfo['version'] = "develop-slow.${buildNumber}-${commit}"
+                    break
+                case ~/rel-([0-9]{2}\.[0-9])/:
+                    publishInfo['publish'] = true
+                    publishInfo['repo'] = 'navapp-releases'
+                    publishInfo['version'] = "${java.util.regex.Matcher.lastMatcher[0][1]}.${buildNumber}-${commit}"
+                    break
+            }
         }
     }
     return publishInfo

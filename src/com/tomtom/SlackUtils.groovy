66b89f30fbe25368c1b36d9409e4a1ba21a8fc26
Erik Vermunt <Erik.Vermunt@tomtom.com>
2019-11-18 12:49:19 +0100

    Always notify slack users regardless of build status

--- src/com/tomtom/SlackUtils.groovy
+++ src/com/tomtom/SlackUtils.groovy
@@ -5,7 +5,6 @@ import hudson.model.Result
 @NonCPS
 static def getSlackConfig(env, currentBuild, rootJob, branchName) {
     def slackConfig = [:]
-    slackConfig.user = null
 
     def channelConfig = [
       "slow-build": [
@@ -20,19 +19,20 @@ static def getSlackConfig(env, currentBuild, rootJob, branchName) {
 
     slackConfig.channel = channelConfig[rootJob]?.get(branchName)
 
-    if (slackConfig.channel == null) {
-        def buildUser = User.get(currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId())
-        if (!buildUser.getProperty(jenkins.plugins.slack.user.SlackUserProperty.class).disableNotifications) {
-            slackConfig.channel = "@${buildUser.getId()}"
-        }
-    }
-
     // Check results
     def passed = currentBuild.resultIsBetterOrEqualTo(Result.SUCCESS.toString())
     def previousPassed = currentBuild.previousBuild == null || currentBuild.previousBuild.resultIsBetterOrEqualTo(Result.SUCCESS.toString())
 
-    // Only notify when build failed or status changed
-    slackConfig.notify = slackConfig.channel != null && (!passed || passed != previousPassed)
+    if (slackConfig.channel == null) {
+        def buildUser = User.get(currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId())
+        if (!buildUser.getProperty(jenkins.plugins.slack.user.SlackUserProperty.class).disableNotifications) {
+            slackConfig.channel = "@${buildUser.getId()}"
+            slackConfig.notify = true
+        }
+    } else {
+        // Only notify channels if build failed or status changed
+        slackConfig.notify = !passed || passed != previousPassed
+    }
 
     if (slackConfig.notify) {
         slackConfig.message = "${BuildUtils.getBuildStatus(currentBuild)} - ${URLDecoder.decode(env.JOB_NAME)} #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
